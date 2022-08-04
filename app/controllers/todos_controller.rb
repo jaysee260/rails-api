7b9819369ada3a9985b3609e5fd89ebdeccdf548
JC Gonzalez <33580723+jaysee260@users.noreply.github.com>
2022-08-04 16:11:51 +0000

    define todo CRUD actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,32 @@
 class TodosController < ApplicationController
+    def index
+        @todos = Todo.all
+        render json: @todos
+    end
+
+    def show
+        @todo = Todo.find(params[:id])
+        render json: @todo
+    end
+
+    def create
+        @todo = Todo.create(title: params[:title])
+        render json: @todo
+    end
+
+    def update
+        @todo = Todo.find(params[:id])
+        @todo.update(
+            title: params[:title]
+            completed: params[:completed]
+        )
+        render json: @todo
+    end
+
+    def destroy
+        @todos = Todo.all
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        render json: @todos
+    end
 end
